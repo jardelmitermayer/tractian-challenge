b5165adff3044209d05d01ed6f9e6ba22abe4f58
Jardel Mitermayer <jardelska@gmail.com>
2024-09-04 00:56:43 -0300

    feat: put icons

--- src/components/ComponentDetails/index.tsx
+++ src/components/ComponentDetails/index.tsx
@@ -1,14 +1,17 @@
 import { FC } from "react";
 import { Component } from "../../types/type";
 
+import './styles.css'
+import { ComponentIcon } from "../Icons/ComponentIcon";
 interface ComponentDetailsProps {
   component: Component;
 }
 
 export const ComponentDetails: FC<ComponentDetailsProps> = ({ component }) => {
   return (
-    <div>
-      <p>{component.name}</p>
+    <div className="component-details">
+      <ComponentIcon />
+      <span className="name">{component.name}</span>
     </div>
   );
 };
--- src/components/TreeLocation/index.tsx
+++ src/components/TreeLocation/index.tsx
@@ -1,7 +1,13 @@
 import { FC, useState } from "react";
-import { Locations } from "../../types/type";
-interface TreeNodeProps {
-  node: Locations;
+import { Asset, Locations } from "../../types/type";
+import { AssetIcon } from "../Icons/AssetIcon";
+import { ComponentIcon } from "../Icons/ComponentIcon";
+import { LocationIcon } from "../Icons/LocationIcon";
+
+import './styles.css'
+
+type TreeNodeProps = {
+  node: Locations | Asset;
 }
 
 export const TreeLocation: FC<TreeNodeProps> = ({ node }) => {
@@ -11,15 +17,27 @@ export const TreeLocation: FC<TreeNodeProps> = ({ node }) => {
       setIsExpanded(prevState => !prevState)
     )
   }
+  const renderIcon = () => {
+    if ('locationId' in node && node.locationId) {
+      return <AssetIcon />;
+    } else if ('sensorType' in node && node.sensorType) {
+      return <ComponentIcon />;
+    } else {
+      return <LocationIcon />;
+    }
+  };
 
   return (
     <div className="tree-node">
-      {node?.children &&
-        <button className="toggle-icon" onClick={toggle}>
-          {isExpanded ? "↑" : "↓"}
-        </button>
-      }
-      <span>{node.name}</span>
+      <div className="container">
+        {node?.children &&
+          <button className="toggle-icon" onClick={toggle}>
+            {isExpanded ? "↑" : "↓"}
+          </button>
+        }
+        {renderIcon()}
+        <span className="name">{node.name}</span>
+      </div>
       {isExpanded && node?.children && (node?.children.map(child => <TreeLocation node={child} />))}
 
     </div>
