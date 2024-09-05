6f42a95469c62215422fa0973446636ffd856ca2
Jardel Mitermayer <jardelska@gmail.com>
2024-09-05 17:31:17 -0300

    style: design fixes

--- src/components/ComponentDetails/index.tsx
+++ src/components/ComponentDetails/index.tsx
@@ -1,14 +1,19 @@
-import { FC, useCallback } from 'react';
+import { FC, useCallback, useState } from 'react';
 import { Asset } from '../../types/type';
 import './styles.css';
 import { CriticalCircle } from '../icons/CriticalCircle';
 import { GreenLighting } from '../icons/GreenLighting';
+import { SensorIcon } from '../icons/SensorIcon';
+import { ReceiverIcon } from '../icons/ReceiverIcon';
 
+import motor from "../../assets/motor.png"
 interface ComponentDetailsProps {
   component: Asset;
 }
 
 export const ComponentDetails: FC<ComponentDetailsProps> = ({ component }) => {
+  const [showImage, setShowImage] = useState<boolean>(false);
+
   const renderComponentIcon = useCallback(() => {
     if (component.status === "operating") {
       return <GreenLighting />;
@@ -16,6 +21,11 @@ export const ComponentDetails: FC<ComponentDetailsProps> = ({ component }) => {
       return <CriticalCircle />;
     }
   }, [component]);
+
+  const getFirstLetter = (name: string) => {
+    return name.charAt(0).toUpperCase();
+  };
+  console.log(component)
   return (
     <div className="component-details">
       <div className="title-status">
@@ -25,15 +35,49 @@ export const ComponentDetails: FC<ComponentDetailsProps> = ({ component }) => {
 
       <div className="component-info">
         <div className="photo-type-owner">
-          <div className="component-image-placeholder">Adicionar imagem do Ativo</div>
+          {showImage ?
+            <img
+              className="component-image"
+              src={motor}
+              alt="component-example"
+              onClick={() => setShowImage(prevState => !prevState)}
+            />
+            : <div
+              className="component-image-placeholder"
+              onClick={() => setShowImage(prevState => !prevState)}>
+              Adicionar imagem do Ativo
+            </div>
+          }
+
           <div className="type-owner">
-            <p><strong>Tipo de Equipamento:</strong> {component.sensorType}</p>
-            <p><strong>Responsáveis:</strong> Eletrica</p>
+            <strong>Tipo de Equipamento</strong>
+            <p> Motor Elétrico (Trifásico)</p>
+            <strong>Responsáveis</strong>
+            <div className="responsaveis">
+              <div className="avatar">
+                {getFirstLetter('Elétrica')}
+              </div>
+              <p>Eletrica</p>
+            </div>
           </div>
         </div>
-
-        <p><strong>Sensor:</strong> {component.sensorId}</p>
-        <p><strong>Receptor:</strong> {component.gatewayId}</p>
+        <div className="divider"></div>
+        <div className='sensor-receiver'>
+          <div>
+            <strong>Sensor</strong>
+            <div className="icon-sensor-id">
+              <SensorIcon />
+              <p> {component.sensorId}</p>
+            </div>
+          </div>
+          <div>
+            <strong>Receptor</strong>
+            <div className="icon-sensor-id">
+              <ReceiverIcon />
+              <p>{component.gatewayId}</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
